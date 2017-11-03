04e351d1b45010eb26392f682782ed891c41ab24
xuan hien <xuanhienhaihau@gmail.com>
2017-11-03 10:32:36 +0700

    xoa

--- demo/Duaxe/js/main.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(1280, 1280, Phaser.AUTO, 'game-area');
-
-game.state.add('Preload', Preload);
-game.state.add('Home', Home);
-game.state.add('GameState', GameState)
-
-game.state.start('Preload');
