10212f2797dacde4b73876d0d9d745edb7bb5624
hien-ict <xuanhienhaihau@gmail.com>
2017-12-28 10:14:32 +0700

    caro java

---
+++ Nhom27/Readme.txt
@@ -0,0 +1,6 @@
+﻿- Mã nguồn được lưu trong file nén src.zip (cùng với hình ảnh 2 quân cờ)
+- Toàn bộ mã nguồn và hình ảnh chung 1 package (package : me)
+- Tiến hành cài chạy chương trình một cách bình thường
+- Hàm main nằm ở class MainView
+- Hoặc chạy trực tiếp Caro.jar
+- Bổ sung luật : chặn 2 đầu không thắng
---
+++ Nhom27/src/me/State.java
@@ -0,0 +1,43 @@
+package me;
+
+import java.awt.Point;
+
+/**
+ * This class describes the state of the a position on the chessboard, including
+ * the position and value of that position
+ *
+ * @version 1.0
+ * @author nhatt_000
+ */
+public class State {
+
+    private Point p;
+    private int val;
+
+    public State(Point p, int val) {
+        this.p = new Point(p);
+        this.val = val;
+    }
+
+    public void Set(Point p, int val) {
+        this.p = new Point(p);
+        this.val = val;
+    }
+
+    public Point getP() {
+        return p;
+    }
+
+    public void setP(Point p) {
+        this.p = p;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+}
